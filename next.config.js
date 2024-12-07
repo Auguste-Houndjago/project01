255224e427249a3e6213a554f63e873e3adb8627
Auguste-Houndjago <augustehoundjago@gmail.com>
2024-12-07 20:08:30 +0000

    up

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: undefined,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-
+  typescript:{
+    ignoreBuildErrors: true
+  },
 };
 
 module.exports = nextConfig;
