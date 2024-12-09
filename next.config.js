ecbb3ea3f458201321ce96e4cc314ef782463ac9
Auguste-Houndjago <augustehoundjago@gmail.com>
2024-12-09 15:23:15 +0000

    up

--- next.config.js
+++ next.config.js
@@ -2,13 +2,9 @@
 const nextConfig = {
   output: undefined,
 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   images: { unoptimized: true },
-  typescript:{
-    ignoreBuildErrors: true
-  },
+
 };
 
 module.exports = nextConfig;
