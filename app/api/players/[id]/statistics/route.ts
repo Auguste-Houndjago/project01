22931045e84b2cfa9d74cdb2a49736557b0a0aa2
Auguste-Houndjago <augustehoundjago@gmail.com>
2024-12-09 15:55:41 +0000

    up

--- app/api/players/[id]/statistics/route.ts
+++ app/api/players/[id]/statistics/route.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// POST method
 export async function POST(
   request: Request,
   { params }: { params: { id: string } }
@@ -12,15 +13,19 @@ export async function POST(
     const statistics = await prisma.statistics.create({
       data: {
         ...body,
-        playerId: params.id
-      }
+        playerId: params.id,
+      },
     });
     return NextResponse.json(statistics);
   } catch (error) {
-    return NextResponse.json({ error: 'Error creating statistics' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Error creating statistics' },
+      { status: 500 }
+    );
   }
 }
 
+// GET method
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
@@ -29,63 +34,49 @@ export async function GET(
     const statistics = await prisma.statistics.findMany({
       where: { playerId: params.id },
     });
-    return NextResponse.json(statistics)
-  }
-  catch (error) {
+    return NextResponse.json(statistics);
+  } catch (error) {
     return NextResponse.json(
-      {
-        error: 'Error fetching statistics'
-      },
+      { error: 'Error fetching statistics' },
       { status: 500 }
-    )
+    );
   }
-
 }
 
+// PUT method
 export async function PUT(
   request: Request,
   { params }: { params: { id: string } }
-
 ) {
-
   try {
-    const body = await request.json()
+    const body = await request.json();
     const updatedStatistics = await prisma.statistics.updateMany({
       where: { playerId: params.id },
-      data: body
-    })
-    return NextResponse.json(updatedStatistics)
-  }
-  catch (error) {
+      data: body,
+    });
+    return NextResponse.json(updatedStatistics);
+  } catch (error) {
     return NextResponse.json(
-      {
-        error: 'error udpating statistics'
-      },
-      {status: 500}
-
-    )
+      { error: 'Error updating statistics' },
+      { status: 500 }
+    );
   }
 }
 
+// DELETE method
 export async function DELETE(
-
-  {params} :{params :{id:string}}
-){
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   try {
     await prisma.statistics.deleteMany({
-      where:{playerId: params.id},
-
-    })
-    return NextResponse.json({message:'Statistics delete successfully'})
+      where: { playerId: params.id },
+    });
+    return NextResponse.json({ message: 'Statistics deleted successfully' });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Error deleting statistics' },
+      { status: 500 }
+    );
   }
-
-  catch(error){
-      return NextResponse.json(
-        {error:'Error deleting statistics'},
-        {status:500}
-      )
-  }
-
-
-
 }
